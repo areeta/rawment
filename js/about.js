215df2512f501842be182624ea99f665701244ca
Vivian Nguyen <vivian.nguyen1127@gmail.com>
2020-12-13 18:51:49 -0800

    parsed and mapped json data

--- js/about.js
+++ js/about.js
@@ -1,21 +1,22 @@
-function parseTweets(runkeeper_tweets) {
+function parseRamenReviews(RAMEN_REVIEWS) {
 	//Do not proceed if no tweets loaded
-	if(runkeeper_tweets === undefined) {
-		window.alert('No tweets returned');
+	if (RAMEN_REVIEWS === undefined) {
+		window.alert('No ramen reviews returned');
 		return;
 	}
 
-	tweet_array = runkeeper_tweets.map(function(tweet) {
-		return new Tweet(tweet.text, tweet.created_at);
+	ramen_reviews_array = RAMEN_REVIEWS.map(function (ramen_review) {
+		return new RamenReview(ramen_review.REVIEW_ID, ramen_review.BRAND, ramen_review.VARIETY, ramen_review.STYLE, ramen_review.COUNTRY, ramen_review.STARS);
 	});
-	
+
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
-	$('#numberTweets').text(tweet_array.length);
-	
+	$('#numberRamenReviews').text(ramen_reviews_array.length);
+
+
 }
 
 //Wait for the DOM to load
-$(document).ready(function() {
-	loadSavedRunkeeperTweets().then(parseTweets);
+$(document).ready(function () {
+	loadRamenReviewData().then(parseRamenReviews);
 });
